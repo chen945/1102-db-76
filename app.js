ce7a60e23bafd431c02849dc321799d3776f7f51
chen945 <207410076@gms.tku.edu.tw>
2022-04-21 11:17:45 +0800

    Add w10 md

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var crown76Router = require("./routes/crown76");
 var crown2_76Router = require("./routes/crown2_76");
-// var hats_76Router = require("./routes/hats_76");
+var hats_76Router = require("./routes/hats_76");
 
 var app = express();
 
