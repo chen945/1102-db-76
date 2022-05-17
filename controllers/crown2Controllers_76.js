f3365319dc8a6437fd3cdaf3ab5faaaf39e938da
chen945 <207410076@gms.tku.edu.tw>
2022-05-17 16:29:27 +0800

    w13-p1: create controllers for crown2 for MVC architecture

---
+++ controllers/crown2Controllers_76.js
@@ -0,0 +1,36 @@
+const Category_76 = require("../models/Category_76");
+const Shop_76 = require("../models/Shop_76");
+
+exports.getCategories = async (req, res) => {
+    try {
+        let results = await Category_76.fetchAll();
+        console.log("results", JSON.stringify(results));
+        res.render("crown2_76/index", {
+            data: results,
+            name: "Doris Hsieh",
+            id: "207410076",
+        });
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+exports.getCategories = async (req, res) => {
+    console.log("category", req.params.category);
+    try {
+        const cid = await Category_76.fetchCatIdByName(req.params.category);
+        // console.log("cid", cid);
+        // const results = Shop_76.fetchProductByCategory(cid);
+        let results = await Shop_76.fetchProductByCategory(cid);
+        console.log("results", JSON.stringify(results));
+
+        res.render("crown2_76/products_76", {
+            data: results,
+            title: req.params.category,
+            name: "Doris Hsieh",
+            id: "207410076",
+        });
+    } catch (err) {
+        console.log(err);
+    }
+};
--- routes/crown2_76.js
+++ routes/crown2_76.js
@@ -3,42 +3,11 @@ var router = express.Router();
 
 const Category_76 = require("../models/Category_76");
 const Shop_76 = require("../models/Shop_76");
+const crown2Controller_76 = require("../controllers/crown2Controllers_76");
 
 /* GET home page. */
-router.get("/", async function (req, res, next) {
-    try {
-        let results = await Category_76.fetchAll();
-        console.log("results", JSON.stringify(results));
-        res.render("crown2_76/index", {
-            data: results,
-            title: req.params.category,
-            name: "Doris Hsieh",
-            id: "207410076",
-        });
-    } catch (err) {
-        console.log(err);
-    }
-});
+router.get("/", crown2Controller_76.getCategories);
 
-router.get("/shop_76/:category", async function (req, res) {
-    console.log("category", req.params.category);
-
-    try {
-        const cid = await Category_76.fetchCatIdByName(req.params.category);
-        // console.log("cid", cid);
-        // const results = Shop_76.fetchProductByCategory(cid);
-        let results = await Shop_76.fetchProductByCategory(cid);
-        console.log("results", JSON.stringify(results));
-
-        res.render("crown2_76/products_76", {
-            data: results,
-            title: req.params.category,
-            name: "Doris Hsieh",
-            id: "207410076",
-        });
-    } catch (err) {
-        console.log(err);
-    }
-});
+router.get("/shop_76/:category", crown2Controller_76.getProductsByCategory);
 
 module.exports = router;
