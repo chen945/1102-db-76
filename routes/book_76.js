641fd9a287c5285bd8682d399bf1d77f38bb9434
chen945 <207410076@gms.tku.edu.tw>
2022-06-07 17:20:47 +0800

    ### w16-p3: /book_xx/create to book_xx/add_xx.ejs

--- routes/book_76.js
+++ routes/book_76.js
@@ -7,14 +7,24 @@ router.get("/", async (req, res) => {
     try {
         const results = await db.query("SELECT * FROM book_76");
         console.log("results", JSON.stringify(results.rows));
-        res.render("book_76/index", {
+        res.render("book_76/", {
             data: results.rows,
             id: "207410076",
             name: "Doris Hsieh",
         });
     } catch (err) {
-        console.log("err", err);
+        res.render("book_76/", {
+            data: "",
+            id: "207410076",
+            name: "Doris Hsieh",
+        });
     }
 });
 
+router.get("/create", (req, res) => {
+    res.render("book_76/add_76", {
+        id: "207410076",
+        name: "Doris Hseih",
+    });
+});
 module.exports = router;
